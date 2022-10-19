0abd032bb41d2c03fdf3d26506c42320daab59ff
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-20 03:13:46 +0530

    car backend app

---
+++ src/main/java/com/nestdigital/carsbackendapp/CarsBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.carsbackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/carsbackendapp/CarsBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.carsbackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
